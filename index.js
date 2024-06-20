b1d8702f8a32f3359b9de22e443c71d70cab5c83
Valdas Bucius <valdasbucius1@gmail.com>
2024-06-20 13:34:27 -0700

    NodeJs Launching server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 
 ////////////////////////
 // Files
@@ -35,3 +36,24 @@ const http = require('http')
 
 ///////////////////////////////////
 // SERVER
+const server = http.createServer((req, res)=>{
+    console.log(req.url)
+
+    const pathName = req.url
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the OVERVIEW')
+    } else if (pathName === '/product'){
+        res.end('This is the PRODUCT')
+    } else {
+        res.writeHead(404, {
+           'Content-type' : 'text/html' ,
+           'my-own-header' : 'hello-world'
+        })
+        res.end('<h1>Page not found</h1>')
+    }
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000')
+})
