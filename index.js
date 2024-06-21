d0b55c28daad3c73e2508fb00aea230e524a5bc6
Valdas Bucius <valdasbucius1@gmail.com>
2024-06-20 18:38:56 -0700

    Replacing html content

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ const url = require('url')
 ////////////////////////
 //Blocking, synchronus way
 
-// const textIn = fs.readFileSync('./txt/input.txt','utf-8')
+const textIn = fs.readFileSync('./txt/input.txt','utf-8')
 
-// console.log(textIn)
+console.log(textIn)
 
-// const textOut = `This is what we know about the avocado : ${textIn}.\nCreated on ${Date.now()}`;
+const textOut = `This is what we know about the avocado : ${textIn}.\nCreated on ${Date.now()}`;
 
-// fs.writeFileSync('./txt/output.txt', textOut)
+fs.writeFileSync('./txt/output.txt', textOut)
 
-// console.log('File written!')
+console.log('File written!')
 
-//////////////
+// //////////////
 // Non-blocking Asynchronus way
 
 // fs.readFile('./txt/starts.txt', 'utf-8', (err, data1) => {
@@ -36,17 +36,52 @@ const url = require('url')
 
 ///////////////////////////////////
 // SERVER
+
+const replaceTemplate = (temp, product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, `not-organic`)
+    return output
+}
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data)
+
+
+
 const server = http.createServer((req, res)=>{
-    console.log(req.url)
 
     const pathName = req.url
 
+    // Overview page
     if(pathName === '/' || pathName === '/overview'){
-        res.end('This is the OVERVIEW')
+
+        res.writeHead(200, {'Content-type' : 'text/html'})
+        const cardsHtml = dataObj.map(obj => replaceTemplate(tempCard, obj)).join('')
+        const output = tempOverview.replace('{%PRODUCT_CARD%}', cardsHtml)
+
+        res.end(output)
+
+    // Product page
     } else if (pathName === '/product'){
         res.end('This is the PRODUCT')
+
+    // API
     } else if (pathName === '/api'){
-        res.end('API')
+        res.writeHead(200, { 'Content-type': 'application/json'})
+        res.end(data)
+
+    // Not Found
     } else {
         res.writeHead(404, {
            'Content-type' : 'text/html' ,
@@ -59,3 +94,6 @@ const server = http.createServer((req, res)=>{
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000')
 })
+
+
+
